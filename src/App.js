cdfda72ad7f8a3a2e0ebc1b1fbb7d559d49f536d
imcod3r <codekickebf@gmail.com>
2022-06-09 23:14:07 -0400

    New NavBar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import NavBar from './Components/NavBar.jsx';
+//@ts-check
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1> Conoce Tu Consumo</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <NavBar/>
+      <h2>Los candidatos</h2>
+    </>
+  
+  )
 }
 
 export default App;
